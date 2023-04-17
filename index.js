fa9db689cab2760c7e51d9daa1bb3d0443193242
Rashedul islam <rashedul360rs@gmail.com>
2023-04-17 11:53:44 +0600

    Update index.js
    health route added

--- index.js
+++ index.js
@@ -73,6 +73,9 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/health", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
